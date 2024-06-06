57f03c0d0ca98338d53212170a6c70251dec0371
Nguyễn Tuấn <anhtuan14072002@gmail.com>
2024-06-06 20:16:38 +0700

    tuấn

--- calligraphy_101.php
+++ calligraphy_101.php
@@ -115,17 +115,85 @@
                 <p><b>3. Find a proper study resource and create an effective practice routine.</b></p>
                 <img src="/images/li_calligraphy101_10.png" alt="">
                 <p>With that in mind, let’s have a closer look at each of these three key steps. </p>
-
-
-
-
-
-
-
-
-
+                <h4>Step 1 – Choosing A Calligraphy Style </h4>
+                <p>The first thing you should do is find the calligraphy style that appeals the most to you.</p>
+                <p>This is the first step since the next step (picking the tools) will depend on your calligraphy style, aka script choice.</p>
+                <img src="/images/li_calligraphy101_11.jpg" alt="">
+                <p>I often get asked what is the easiest calligraphy style to start with, and the truth is that each style has its little nuances that will create challenges along the way.</p>
+                <p>Some people might find working with a pointed nib easier, while others find it more comfortable with a broad-edged pen.</p>
+                <p>To learn, understand, and properly execute a specific calligraphy style, you need to stick to it for an extended period, so it’s essential that you choose something that is appealing to you. </p>
+                <p>Generally speaking, there are a few main categories of calligraphy styles.</p>
+                <p>We can categorize calligraphy styles according to the type of pen we use. The main categories are – </p>
+                <p>
+                    <ul>
+                        <li>Pointed nib calligraphy styles (Copperplate, Spencerian, etc.)</li>
+                        <li>Broad-edged calligraphy styles (Uncial, Gothic, Foundational, etc.)</li>
+                        <li>Brush pen calligraphy </li>
+                    </ul>
+                </p>
+                <img src="/images/li_calligraphy101_12.png" alt="">
+                <p>To make it easier for you, I created a separate guide on the 10 best calligraphy styles for beginners.</p>
+                <p>That article contains the most popular calligraphy styles and many helpful resources to help you make the right first step</p>
+                <p>So if you’re a complete beginner, make sure to check out that article first.</p>
+                <p><i><b>Quick note:</b> The different calligraphy styles are usually referred to as scripts. People commonly use the term “calligraphy fonts,” but that is incorrect. Fonts are what (today) computers use to display text.</i></p>
+                <h4>Step 2 – Picking The Correct Calligraphy Pen + The Essential Calligraphy Tools</h4>
+                <p>Remember, not every calligraphy pen suits every style.</p>
+                <p>This is why it’s crucial to pick the style and then the calligraphy pen that fits that style.</p>
+                <p>Entry-level pens and tools are not crazy expensive, but it would be silly to waste your money simply because you didn’t know what you needed to get started.</p>
+                <img src="/images/li_calligraphy101_13.jpg" alt="">
+                <p>Luckily, with one pen, you can create a bunch of different styles.</p>
+                <p>On top of that, there are only three main pen categories to choose from.</p>
+                <p>To help you find the right pick, I have created a complete in-depth guide on choosing the right calligraphy pen. s</p>
+                <p>In addition to the calligraphy pen, you will also need the basic calligraphy tools.</p>
+                <p>The basic calligraphy tools are :</p>
+                <p>
+                    <ul>
+                        <li>The calligraphy pen</li>
+                        <li>Paper</li>
+                        <li>Ruler</li>
+                        <li>Pencil</li>
+                        <li>Eraser</li>
+                        <li>Calligraphy ink</li>
+                    </ul>
+                </p>
+                <img src="/images/li_calligraphy101_14.jpg" alt="">
+                <p>To learn more about this, I created a separate article on the 6 essential calligraphy tools. </p>
+                <p>By following these two guides, you will find the correct high-quality tools without wasting your time or breaking the bank. </p>
+                <h4>What If You Cannot Afford Or Find Any Calligraphy Tools In Your Area?</h4>
+                <p>Fear not!</p>
+                <p>I wrote a tutorial where I show you how to create calligraphy with a regular pen, and if you have a pencil lying around, you can also do calligraphy using just a pencil. </p>
+                <img src="/images/li_calligraphy101_15.jpg" alt="">
+                <p>Other articles about calligraphy tools you might be interested in checking out :</p>
+                <h4>Step 3 – Find A Proper Study Resource And Create An Effective Practice Routine.</h4>
+                <p>Once you’ve picked out the calligraphy style and the needed tools, it’s time to find a proper resource and start practicing.</p>
+                <p>One of the best ways to learn calligraphy is by attending an in-person workshop hosted by a professional calligrapher.</p>
+                <p>An online workshop is also great. </p>
+                <p>However, you can still find a bunch of great free resources online.</p>
+                <p>Instructional calligraphy books are also fantastic – I’ll talk more about them later on.</p>
+                <img src="/images/li_calligraphy101_16.jpg" alt="">
+                <p>With the help of a good study resource, you’ll learn how to construct letterforms properly and keep your calligraphy proportional, consistent, and well-balanced.</p>
+                <p>Basically, all the elements you need to develop a strong foundation upon which you can then easily build more complex elements and learn new calligraphy styles.</p>
+                <p>You could just find an alphabet from Google Images or Pinterest and copy that for several months, but you might do more damage than good with that approach.</p>
+                <p>It’s much better to practice with a correctly structured exemplar that has a ductus along with it.</p>
+                <p>An exemplar is a full alphabet demonstration for you to use as a reference.</p>
+                <p>The ducts are the little red arrows indicating the stroke sequence to construct the letters. You can see an example of it below.</p>
+                <img src="/images/li_calligraphy101_17.png" alt="">
+                <p>Trust me when I say this – finding a good study resource could make a massive difference in the quality of your calligraphy artwork and how quickly you can learn and improve your skills. </p>
+                <h4>Creating An Effective Practice Routine.</h4>
+                <p>Calligraphy is a motor skill.</p>
+                <p>This means that the brain, muscles, and nervous system all need to work together.</p>
+                <p>It also means that building muscle memory requires a lot of practice.</p>
+                <p>In other words, the more you practice, the better you will become at calligraphy.</p>
+                <p>If you’ve been reading my articles or will be reading them, you will often see me repeat the following sentence: Consistent practice is important, but how you practice makes all the difference.</p>
+                <img src="/images/li_calligraphy101_18.jpg" alt="">
+                <p>Over the years, I’ve seen some people progress more in six months than others in several years.</p>
+                <p>The key difference was almost always the way they practiced. </p>
+                <p>I cannot stress enough how important it is to create a good and effective calligraphy practice routine and how big of an impact it can have on your progress. </p>
+                <p>I also want to point out that it’s important that you make the routine fun.</p>
+                <p>Aimlessly copying basic strokes and alphabets can quickly become tedious</p>
+                <p>In that article, I share many helpful tips and a suggested practice approach that works for any calligraphy style you choose to practice. </p>
+                <p>On top of that, you can check out my article on the most common calligraphy mistakes and how to avoid them. </p>
             </div>
-
         </div>
         <p></p>
     </div>
