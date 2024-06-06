78cd4f6017808ea1049e6f330b70886f32f36cce
Nguyễn Tuấn <anhtuan14072002@gmail.com>
2024-06-06 19:18:31 +0700

    tuấn

---
+++ calligraphy_101.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/navbar2.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/detail_library.css">
+    <?php include_once("styles/styles.php")?>
+    <title>About</title>
+</head>
+<body>
+    <div class="nav">
+        <?php include_once("header_footer/navbar2.php")?>
+    </div>
+    <div class="container">
+        <div class="calligraphy_101">
+            <div class="calligraphy_101_1">
+                <h2>Calligraphy 101 – The ULTIMATE Guide For Beginners</h2>
+                <img src="/images/li_calligraphy101.png" alt="">
+                <p>
+                    <br>
+                <i>This post and the photos within it may contain affiliate links. If you purchase something through the link, I may receive a commission at no extra charge to you.</i>
+                </p>
+            </div>
+            <div class="calligraphy_101_2">
+                <p>If you want to learn how to start with calligraphy, you’ve come to the right place.
+                On this page, you will find everything you need to know about what calligraphy is and how to get started with it.
+                </p>
+                <p>Let’s get started!</p>
+                <h4>What is calligraphy? In simple words.</h4>
+                <p>Calligraphy is a visual art form that creates decorative handwriting using specific tools.</p>
+                <p>In other words, calligraphy is the art of beautiful writing.</p>
+                <img src="/images/li_calligraphy101.png" alt="">
+                <p>However, calligraphy is a broad term that encompasses a wide variety of styles and cultures from every corner of the globe. 
+                </p>
+                <p>We can primarily divide calligraphy into different cultures around the world.</p>
+                <p>To name a few – </p>
+                <ul>
+                    <li>Western calligraphy (the Latin alphabet)</li>
+                    <li>Chinese calligraphy</li>
+                    <li>Japanese calligraphy</li>
+                    <li>Arabic calligraphy</li>
+                    <li>Indian calligraphy</li>
+                </ul>
+                <img src="/images/li_calligraphy101_1.png" alt="">
+
+            </div>
+
+        </div>
+        <p></p>
+    </div>
+    <div class="footer">
+        <?php include_once("header_footer/footer.php")?>
+    </div>
+</body>
+</html>
