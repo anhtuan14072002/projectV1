3b3b0b178885f9c88c07dbfbc1f0a2896352a9cf
Nguyễn Tuấn <anhtuan14072002@gmail.com>
2024-06-06 19:53:38 +0700

    tuấn

--- calligraphy_101.php
+++ calligraphy_101.php
@@ -51,6 +51,79 @@
                 
                 <p>It is common for the untrained eye to see these two as the same.</p>
                 <p>With that in mind, here are a few key features of calligraphy. </p>
+                <h4>1. Variation in stroke thickness.</h4>
+                <p>Calligraphy can be done with any writing tool; however, there are specific tools dedicated to calligraphy.</p>
+                <p>The two most common tools are broad-edged and pointed pens.</p>
+                <p>These tools are specific for calligraphy because they can create stroke thickness variations.</p>
+                <p>The broad-edged nib achieves that by keeping the pen at a consistent angle (and the direction of movement), whereas the pointed nib achieves that by adding/removing pressure as you write.</p>
+                <p>The contrast in stroke thickness that these tools can create while you write is one of the most prominent features of calligraphic letters. </p>
+                <img src="/images/li_calligraphy101_2.png" alt="">
+                <h4>2. Letterform proportions and structure.</h4>
+                <p>A specific proportion and structure of letters are maintained throughout the writing.</p>
+                <p>This is particularly important for traditional calligraphy scripts.</p>
+                <p>For example, Blackletter calligraphy is created with a broad-edged tool.</p>
+                <p>The angle of the pen is held at a 45-degree angle.</p>
+                <p>The letters are completely vertical, and they are proportionally sized according to the width of the pen.</p>
+                <p>That’s why we use the pen width as a measuring unit when it comes to broad-edged (flat tip) calligraphy styles.</p>
+                <p>These are just some of the “rules” you need to follow to create this particular style of calligraphy.</p>
+                <p><b>Calligraphy guidelines</b> allow us to focus on creating beautiful and consistent letterforms.</p>
+                <p>Without calligraphy guidelines, you would have to guess and eyeball all the sizing and proportions, and it would make the whole process much more difficult.</p>
+                <img src="/images/li_calligraphy101_4.png" alt="">
+                <h4>3. Consistency and uniformity.</h4>
+                <p>Calligraphy places great emphasis on consistent and uniform letterforms.</p>
+                <p>Achieving consistent stroke widths, angles, spacing, rhythm, etc., and overall letter shapes throughout a piece of writing is essential for creating a cohesive and visually pleasing result.</p>
+                <p>In the example below, I am using two different colored brush pens along with the <b>basic calligraphy strokes</b> to write out the word minimum.</p>
+                <p>I am focused on maintaining consistent spacing, slant, space within the letters (also known as counters), etc.</p>
+                <img src="/images/li_calligraphy101_5.jpg" alt="">
+                <p><i>The word “minimum” is a popular practice word for calligraphers since it contains many similar shapes.</i></p>
+                <h4>What is calligraphy used for?</h4>
+                <p>Today, calligraphy is used in many different ways.</p>
+                <p>From personal use as a relaxing hobby and artistic expression to commercial purposes, official documents, wedding invitations, font designs, and much more. </p>
+                <p><b>The written word can be used to convey not only information but also emotions.</b></p>
+                <p>As you can see in the image below, the same sentence written in two different styles can communicate a very different feeling.</p>
+                <img src="/images/li_calligraphy101_6.png" alt="">
+                <p>Often we don’t pay any attention to letters used in advertisements, logotypes, and other promotional materials.</p>
+                <p>Try to pay more attention the next time you’re in the city or any other place where you can see letters around you.</p>
+                <p>Take a moment to look at the letters and think about what they communicate on an emotional level and how they make you feel.</p>
+                <p>This could also be a helpful exercise to determine what sort of style of calligraphy you want to learn and practice.</p>
+                <p>At this point, it’s also important to mention that calligraphy and hand lettering are not the same thing.</p>
+                <p>Despite the fact that these two terms are often used interchangeably.</p>
+                <p>When doing calligraphy, you’re <b>writing</b> letters, whereas, with hand lettering, you <b>draw</b> them.</p>
+                <p>I wrote an article about <b> difference between hand lettering and calligraphy.</b> Check it out!</p>
+                <img src="/images/li_calligraphy101_7.jpg" alt="">
+                <h4>The Origins Of Calligraphy</h4>
+                <p>Paleographers and historians believe the first alphabet evolved in Phoenicia around 1200 B.C.</p>
+                <img src="/images/li_calligraphy101_8.png" alt="">
+                <p>As Phoenicians merchants traveled and traded across the Mediterranean, the Greeks adopted their alphabet around the eighth century B.C.</p>
+                <p>Soon after, the Etruscans adopted (and adapted) the Phoenician alphabet, which the Romans subsequently took.</p>
+                <p>By the first century B.C., the Romans had already developed several scripts using the Latin alphabet, which we all know and use today.</p>
+                <p>The word calligraphy derives from the Greek words “kallos” (beauty) and “graphein” (to write).</p>
+                <p>According to Britannica.com, the word calligraphy entered most European languages by the end of the 16th century.</p>
+                <p>This was initiated by the introduction of the printing press in the mid-15th century and thus created a clear distinction between handwriting and more elaborate forms of lettering.</p>
+                <p>We often take it for granted, but the written word is one of the foundations of human civilization.</p>
+                <p>Writing has allowed us to share and preserve information, ideas, and knowledge for thousands of years.</p>
+                <p>The history of calligraphy is rich, with many evolutions throughout centuries.</p>
+                <p>Below you can see a shorter general overview of how the Latin alphabet evolved throughout history.</p>
+                <img src="/images/li_calligraphy101_9.png" alt="">
+                <p>In case you want to learn more about the history of calligraphy, I’ve written a separate in-depth article on the history of calligraphy and how it evolved throughout the years. </p>
+                <h4>How To Get Started With Calligraphy? The Three-Step Formula</h4>
+                <p>Ok, now we know what calligraphy is, what it is used for, and where it comes from.</p>
+                <p>Now it’s time to learn how to start learning and practicing calligraphy.</p>
+                <p>If you’re a complete beginner, the best way to start learning calligraphy is by following the three-step formula :</p>
+                <p><b>1. Choose a calligraphy style that you want to study and practice.</b></p>
+                <p><b>2. Pick the correct calligraphy pen for the style you want to practice + the essential calligraphy tools.</b></p>
+                <p><b>3. Find a proper study resource and create an effective practice routine.</b></p>
+                <img src="/images/li_calligraphy101_10.png" alt="">
+                <p>With that in mind, let’s have a closer look at each of these three key steps. </p>
+
+
+
+
+
+
+
+
+
             </div>
 
         </div>
