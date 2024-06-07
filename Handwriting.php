461cd058c0292f7daaff71634c70a17430ffa597
NguyenDucTai <vhuy12991@gmail.com>
2024-06-07 21:04:14 +0700

    page5

---
+++ Handwriting.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/navbar2.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/detail_library.css">
+    <?php include_once("styles/styles.php")?>
+    <title>About</title>
+</head>
+<body>
+    <div class="nav">
+        <?php include_once("header_footer/navbar2.php")?>
+    </div>
+    <div class="container">
+        <div class="calligraphy_101">
+            <div class="calligraphy_101_1">
+                <h2>How To Make Handwritten Fonts on Your iPad</h2>
+                <img src="images\HowToMake.png" alt="">
+                <p>
+                    <br>
+                <i>This post and the photos within it may contain affiliate links. If you purchase something through the link, I may receive a commission at no extra charge to you.</i>
+                </p>
+            </div>
+            <div class="calligraphy_101_2">
+                <p>In this article, I will show you how to create custom hand-drawn and handwritten fonts using your iPad and the Fontself app. </p>
+                <p>The process is super easy and intuitive.</p>
+                <p>Even if you have 0 experience with font making, you’ll be able to turn your <b>lettering, calligraphy,</b> or <b>handwriting</b> into a fully working font. </p>
+                <p>Here is a quick overview of the tutorial – </p>
+
+                <p>1. <b>Tools needed to create the font</b></p>
+                <p>2. <b>A quick tour of the Fontself app </b></p>
+                <p>3. <b>Step-by-step process to make the font</b></p>
+                <p>4. <b>Final overview of the Fontself app </b></p>
+                <p>Note – this article was sponsored by Fontself, but all opinions are my own. </p>
+                <p>In case you prefer to watch, I also create a YouTube video – </p>
+                <iframe width="640" height="360" src="https://www.youtube.com/embed/fXQINgq0yS8" title="How To Make Fonts On Your iPad (Fontself Review)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                
+                <h3>Tools needed to create the font</h3>
+                <p>To create my font, ill be using the <<b>iPad Pro</b> and the <b>Apple Pencil.</b></p>
+                <p>I’ll also use the Procreate app for sketching and, of course, the Fontself app to create my font. </p>
+                <p>You can download the Fontself app onto your iPad for free using this link – <a href=" https://apple.co/3HoNN4k"> https://apple.co/3HoNN4k</a></p>
+                <p>If you don’t have Procreate, you can also sketch your letters on paper or draw the letters straight into Fontself.</p> 
+                <img src="images\li-howtomake.jpg" alt="">
+
+                <h3>A quick tour of the Fontself app </h3>
+                <p>Fontself originally started as an Illustrator and Photoshop extension,<b>which adds font creation features to Adobe’s software.</b></p>
+                <p>which adds font creation features to Adobe’s software.</p>
+                <p>And for those, I always used Fontself as it was super affordable and intuitive to use.</p>
+                <img src="images\li-howtomake1.jpg" alt="">
+                <p>Well, Fontself is now broadening its focus toward the iPad. Making font creation even easier and more fun than ever before. </p>
+                <p>This Fontself app is free to download and use. </p>
+                <p>There is a one-time fee of a little above 10$ to export fonts, but that fee allows you to export as many fonts as you want.</p>
+                <p>It will also unlock other cool features I’ll show you later. </p>
+                <p>Once you download the app and open it, you’re greeted with a minimalist interface. </p>
+                <img src="images\li-howtomake2.jpg" alt="">
+                <p>You have several style templates to choose from, or you can choose a clean one with just guidelines. </p>
+                <p>Underneath, you can see the fonts that you have already created. </p>
+                <p>For the font I’m going to create, I’ll select the Sans template.</p>
+                <p>Now you can see the whole alphabet with a few options on all sides of the screen.</p>
+                <img src="images\li-howtomake3.jpg" alt="">
+                <p>On the top left corner, you can tweak some settings for the guidelines by tapping the little wheel icon.</p>
+                <p>I like to keep all the settings turned on. </p>
+                <img src="images\li-howtomake4.jpg" alt="">
+                <p>On the top right side, we have a selection tool which is absolutely fantastic, and I love it.</p>
+                <p>I’ll show you how it works once I start creating the font. </p>
+                <img src="images\li-howtomake3.jpg" alt="">
+                <p>Next up are the brushes.</p>
+                <p>Not a super broad selection, but for our needs – more than enough. </p>
+                <p>The brush settings are very simple yet similar to the ones in Procreate. </p>
+                <p>Definitely, something I really liked. </p>
+                <img src="images\li-howtomake5.jpg" alt="">
+                <p>Next up is the eraser, followed by spacing settings. </p>
+                <p>The spacing of your font is made automatically, and rather than controlling the spacing of each individual character (like you can in the Photoshop and Illustrator extensions), you can adjust the settings as a whole.</p>
+                <p>Both for letters and words. </p>
+                <img src="images\li-howtomake6.jpg" alt="">
+                <p>It works pretty well, but we’ll see that better once I create the font. </p>
+                <p>Finally, are the export and showcasing options plus a few quick guides on how to install your fonts on other apps. </p>
+                <p>Which is also a super cool feature. </p>
+                <img src="images\li-howtomake7.jpg" alt="">
+                <p>Once I finish my font, I’ll show you more of that. </p>
+                <p>The last menu on the right allows you to quickly scroll through the capitals, minuscules, numbers, and glyphs.  </p>
+                <p>You can also select and deselect various languages and categories from this menu. </p>
+                <img src="images\li-howtomake8.jpg" alt="">
+                <p>Another cool feature appears once you start writing the letters. </p>
+                <p>As soon as I put down a letter, you’ll see the famous pangram appearing that says – <b>The quick brown fox jumps over the lazy dog. </b></p>
+                <p>It’s a popular sentence used to display fonts since it contains all the letters of the alphabet. </p>
+                <p>As you keep writing the letters, they appear in the sentence above. </p>
+                <img src="images\li-howtomake9.jpg" alt="">
+                <p>The same goes for the lowercase letters, aka minuscules. </p>
+                <p>Ok, that covers it, and I think we’re ready to start drawing our font. </p>
+                
+                <h3>Step-by-step process to make the font.</h3>
+                <p>The first thing we need to do is determine what type of font we’re going to create. </p>
+                <p>I’ve already created a few fonts using my handwriting, like this right here, but for this article, I wanted to challenge myself a bit more and make a hand-lettered font from scratch. </p>
+
+                <h3>Step 1 – Finding inspiration for your font</h3>
+                <p>One of my favorite places to search for inspiration is Pinterest. </p>
+                <p>I didn’t just find some inspiration. I found an overwhelming amount of inspiration. </p>
+                <p>LIKE A LOT. </p>
+                <p>I was super amazed by the massive variety of shapes and styles. </p>
+                <img src="images\li-howtomake10.jpg" alt="">
+                <p>If you want to do the same, check out my Pinterest board, where you’ll find all the images I saved.</p>
+                <p>While cruising through endless styles and shapes, I decided to create a high-contrast display font. </p>
+                <p>Display fonts are usually created with the intent to be used on larger sizes for titles and headings and not so much for longer chunks of text. </p>
+                <img src="images\li-howtomake11.jpg" alt="">
+                <p>
+                One big reason I decided to go with a display font was that they look funkier, contain more variability, and, I guess creative freedom. </p>
+                <p>With that in mind, I decided to create only capital letters. </p>
+
+                <h3>Step 2 – Sketching your letters</h3>
+                <p>I took my notes and ideas, opened the Procreate app, and started sketching. </p>
+                <p>Sketching is the most crucial step of the whole process. </p>
+                <p>Sketching (especially with a pencil) allows you to quickly and roughly lay down various ideas</p>
+                <p>Often, even after 10-15 minutes of sketching, you surprise yourself with new ideas you didn’t have at the beginning of the process. </p>
+                <p>I HIGHLY recommend you don’t skip this step. </p>
+                <p>Here is my sketch for the font.</p>
+                <img src="images\li-howtomake12.jpg" alt="">
+                <p>You don’t need to Procreate for this. You can also sketch on paper.</p>
+                <p>Or, if you’re super confident about the font style you want to create, you can start drawing your letters directly into Fontself. </p>
+
+                <h3>Step 3 – Set up your sketch as a reference</h3>
+                <p>Having a visual reference of your sketch in front of you will be of great help. </p>
+                <p>This way, you don’t have to stop, go back, find your sketch, etc. You can get into a flow state and create the whole font rather quickly. </p>
+                <p>The way I did this was by printing out the font. </p>
+                <img src="images\li-howtomake13.jpg" alt="">
+                <p>If you don’t have a printer, you could open up the image on your computer screen or phone. </p>
+                <p>If you sketched your letters on paper, keep the paper in front of you. </p>
+
+                <h3>If you sketched your letters on paper, keep the paper in front of you. </h3>
+                <p>A lot of letters share similar parts, and by copying/pasting these parts, we make the process more efficient and increase the consistency of our letters. </p>
+                <p>Thankfully the awesome selection tool in Fontself allows you to do exactly that. </p>
+                <p>The most frequent part I can identify is the thick stem which appears in most letters. </p>
+                <img src="images\li-howtomake14.jpg" alt="">
+                <p>So, I will double tap on the square of the letter B, which will open a larger window allowing me to focus solely on that letter. </p>
+                <p>A great feature for adding more details. </p>
+                <p>I’m going to draw a line and hold it, which will trigger the automatic shape feature. The same way you would do it in Procreate.</p>
+                <img src="images\li-howtomake15.jpg" alt="">
+                <p>It works well with other shapes, and it’s super helpful to get nice clean lines. </p>
+                <p>I add the little rounded serifs on each side, and I’ll fill the shape with color by simply tapping and holding onto it. </p>
+                <img src="images\li-howtomake16.jpg" alt="">
+                <p>Another handy feature. </p>
+                <p>I can quickly undo and redo each stroke. Two fingers for undoing and three for a redo. Again, just as in Procreate. </p>
+                <p>Ok, so I have my shape. Now, I’ll grab the selection tool, select the object, and copy it. </p>
+                <p>The selection tool also allows you to move objects around, increase their size, rotate them, etc.</p>
+                <p>Ok, once I’ve copied my stem, I’ll start pasting the shape in all the letter boxes that will use that shape in one way or another. </p>
+                <img src="images\li-howtomake17.jpg" alt="">
+                <p>I can also copy and paste similar letters such as the O, G, C, Q, etc. </p>
+                <p>Of course, I don’t want to copy and paste every single detail; otherwise, I lose the organic touch that a hand-lettered font should have. </p>
+
+                <h3>Step 5 – Create the rest of the letters</h3>
+                <p>Now that we have copied and pasted the most frequently used parts, we can focus on creating all the other unique pieces and details. </p>
+                <p>Keep an eye on proportions as well as the counter spaces.</p>
+                <p>In the YouTube video, you can see the whole process.</p>
+                <img src="images\li-howtomake18.jpg" alt="">
+                <p>Having the reference image of your sketch will be of great help in this step. </p>
+                <p>As I keep creating these letters, you can notice in the above preview how they’re starting to show and form the pangram – the quick brown fox jumps over the lazy dog. </p>
+
+                <h3>Step 6 – Adjust the spacing of your letters</h3>
+                <p>Good. Once you’re done drawing/writing your letters, it’s time to adjust the spacing. </p>
+                <p>As mentioned, the spacing is controlled by a single setting for all letters rather than individually. </p>
+                <img src="images\li-howtomake20.jpg" alt="">
+                <p>You can also adjust the spacing between the words. </p>
+
+                <h3>Step 7 – Showcasing the font and exporting it</h3>
+                <p>All that is left to do is check out what your font looks like and export it. </p>
+                <p>As mentioned earlier, Fontself has these features built-in within the app. </p>
+                <p>The showcasing options will allow you to display your font in premade graphics. </p>
+                <img src="images\li-howtomake21.jpg" alt="">
+                <p>You can change the settings of these graphics and export them in various sizes and formats ready for sharing on social media platforms. </p>
+                <img src="images\li-howtomake22.jpg" alt="">
+                <p>This is a fantastic feature as it allows you to see what your font looks like and share it with the community. </p>
+                <p>You’ll also find a few mini-tutorials on how to export and install your new font on various apps. </p>
+                <img src="images\li-howtomake23.jpg" alt="">
+                <p>These are premium features that you’ll unlock with that 10$ one-time fee I mentioned at the beginning of the article. </p>
+                <p>If everything works great, it’s time to hit the export button.</p>
+                <p>The final step is naming your font, and that’s it! </p>
+                <img src="images\li-howtomake24.jpg" alt="">
+
+                <h3>Final overview of the Fontself app </h3>
+                <p>Let’s begin with the things that I like about Fontself. </p>
+                <p>It’s fun, easy, and intuitive to use. </p>
+                <p>I like that you can do everything within the app, and the fact that it requires literally 0 experience to create a fully working font is amazing. </p>
+                <p>It’s also very affordable. It’s free to use, and you just need to pay a one-time fee of a little above 10$ to export as many fonts as you want. </p>
+                <p>There are many really cool features that can streamline your workflow.</p>
+                <p>Such as – </p>
+                <ul>
+                    <li>The selection tool</li>
+                    <li>The copy-paste feature</li>
+                    <li>Being able to see a real-time preview</li>
+                    <li>The automatic shapes</li>
+                    <li>And so much more. </li>
+                </ul>
+                <p>The style templates are also great for getting started. </p>
+                <p>I also really like the fact that many things are similar to Procreate. It made me feel like I’ve used the app before despite it being my first time working with it. </p>
+                <img src="images\li-howtomake25.jpg" alt="">
+                <p>This iPad app is fairly new, and it already has a ton of updates and improvements, and a lot of new and cool stuff is coming up. </p>
+                <p>The creators are awesome and listen to the feedback from their users. </p>
+                <p>As far as things I would like to see improve, here are a few things. </p>
+                <ul>
+                    <li>Slant lines for cursive & italic fonts</li>
+                    <li>Importing premade sketches </li>
+                    <li>A few more brushes. Maybe some with texture.</li>
+                    <li>Fonts in color and textured SVG fonts</li>
+                </ul>
+                <img src="images\li-howtomake26.jpg" alt="">
+                <p>All of these things I mentioned are not deal breakers by any means.</p>
+                <p>These are just some thoughts I gathered while using Fontself. </p>
+                <p>I already shared my feedback with the creators, and I’m without a doubt that it’s just a matter of time before these things come to life.</p>
+
+                <h4>Final Words </h4>
+                <p>And there you have it, folks!</p>
+                <p>That’s how you can create handwritten and hand-drawn fonts using the iPad and the Fontself app. </p>
+                <p>All in all, it’s a fantastic app suited for both beginners and more seasoned lettering artists. </p>
+                <p>Give it a spin, make a font, and I assure you that you won’t regret it. </p>
+                <p>Now let me ask you something – If you would create a font, in what style would you make it? </p>
+                <p>Let me know by dropping a comment below. </p>
+                <p>Until the next one,</p>
+                <p>Stay AWESOME!</p>
+
+                <h3>Pin me!</h3>
+            </div>
+        </div>
+        <p></p>
+    </div>
+    <div class="footer">
+        <?php include_once("header_footer/footer.php")?>
+    </div>
+</body>
+</html>
