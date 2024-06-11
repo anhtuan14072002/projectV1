728cfd07d36d3014acac09978f5f1d5a1132955d
NguyenDucTai <vhuy12991@gmail.com>
2024-06-11 13:47:40 +0700

    fb

---
+++ 25BestHandwriting.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/navbar2.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/detail_library.css">
+    <?php include_once("styles/styles.php")?>
+    <title>About</title>
+</head>
+<body>
+    <div class="nav">
+        <?php include_once("header_footer/navbar2.php")?>
+    </div>
+    <div class="container">
+        <div class="calligraphy_101">
+            <div class="calligraphy_101_1">
+                <h2>25 Best Handwriting Fonts For A Personal Touch</h2>
+                <img src="images\25BestHandwriting.jpg" alt="">
+                <p>
+                    <br>
+                <i>This post and the photos within it may contain affiliate links. If you purchase something through the link, I may receive a commission at no extra charge to you.</i>
+                </p>
+            </div>
+            <div class="calligraphy_101_2">
+                <p>Are you looking for the best handwritten fonts on the market?</p>
+                <p>You’ve come to the right place.</p>
+                <p>In this article, I will review the 25 best handwriting fonts that will instantly level up your designs.</p>
+                <p>You will find handwritten fonts in a variety of styles, such as –</p>
+                <p>Modern, elegant, <b>calligraph</b>y, retro, signature, whimsy, and so much more.</p>
+                <p>The best part?</p>
+                <p>Super affordable, along with all the licensing needed for both personal and commercial products.</p>
+                <p>Make sure to stick to the end for a few bonus tips on how to choose and use handwriting fonts for your projects.</p>
+                <p>Without any further delays,</p>
+                <p>Let’s get started!</p>
+                <p><b>Quick note -</b> Check out also the 71 Best calligraphy fonts article.</p>
+
+                <h3>1. Mythshire – Vintage Handscript Font</h3>
+                <img src="images\25BestHandwriting1.jpg" alt="">
+                <p>Mythshire is a blend of cursive and personality-rich handwriting styles that gives you a vintage vibe. So for all the vintage-inspired branding, logos, wedding invites, book covers, etc., this authentically flowing font is your one-stop place. </p>
+                <h4>The Mythshire font includes – </h4>
+                <ul>
+                    <li>Mythshire .OTF, .TTF, .WOFF</li>
+                    <li>40+ Ligatures   </li>
+                    <li>Numerals and punctuation</li>
+                    <li>Full alternates for both upper and lowercase</li>
+                    <li>14 Ink Splats (in .PSD and Photoshop .ABR brush format)</li>
+                    <li>8 High resolution paper textures (.JPG 6000 x 4000px)</li>
+                </ul>
+
+                <h3>2. Madina – Cursive Handwriting Font</h3>
+                <img src="images\25BestHandwriting2.jpg" alt="">
+                <p>Madina is a cursive font that embraces the modern brush-lettered bouncy strokes to create an exquisite & fun look. With plenty of variations in the letter heights and lots of bouncy curves and loops, this cursive handwriting font serves you well in any situation where your goal is to communicate fun and carefree feeling in your designs. </p>
+                <h4>Included in this set:</h4>
+            </div>
+        </div>
+        <p></p>
+    </div>
+    <div class="footer">
+        <?php include_once("header_footer/footer.php")?>
+    </div>
+</body>
+</html>
