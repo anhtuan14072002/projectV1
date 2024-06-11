6084b56a7695bb724bfc0cdd47437b6dd6db34f7
NguyenDucTai <vhuy12991@gmail.com>
2024-06-11 14:45:45 +0700

    book

--- 25BestHandwriting.php
+++ 25BestHandwriting.php
@@ -53,6 +53,75 @@
                 <img src="images\25BestHandwriting2.jpg" alt="">
                 <p>Madina is a cursive font that embraces the modern brush-lettered bouncy strokes to create an exquisite & fun look. With plenty of variations in the letter heights and lots of bouncy curves and loops, this cursive handwriting font serves you well in any situation where your goal is to communicate fun and carefree feeling in your designs. </p>
                 <h4>Included in this set:</h4>
+                <p>• Madina Script – A handwritten script font containing upper & lowercase characters, numerals, and an extensive range of punctuation.</p>
+                <p>• Madina Script Alt is the second version of Madina Script, with a new set of lowercase characters.</p>
+                <p>• Madina Script Ornaments includes 30 hand-drawn elements, including end-swashes for first and last letters, olive branches, and flourishes.</p>
+                <p>• Madina Clean, Madina Clean Alt & Madina Clean Ornaments – Clean versions (no texture) of the above three fonts.</p>
+                <h3>
+                3. Bayshore – 1980’s Inspired Handwriting Font
+                </h3>
+                <img src="images\25BestHandwriting3.jpg" alt="">
+                <p>Bayshore is a font that is inspired by the 1980s in every way. It has a hand-drawn monoline font look with plenty of flairs. This handwriting font is quite popular in the 1980’s hair care products and TV shows. An excellent choice for old-school-inspired designs and modern adaptations with a handwriting look. </p>
+                <h4>Bayshore offers – </h4>
+                <ul>
+                    <li>Language support</li>
+                    <li>End swashes for each lowercase letter </li>
+                    <li>Underline swashes for that extra style</li>
+                    <li>10 Neon glow Photoshop actions
+                    </li>
+                </ul>
+                <h3>4. Romantically – A Messy Handsript</h3>
+                <img src="images\25BestHandwriting4.jpg" alt="">
+                <p>Want to bring a personal touch to your branding project with a messy handwriting font? Say no more. I am presenting you the Romantically handwriting font. Romantically really gives you a handwritten look, but it manages to retain a high level of readability and classiness. Combined with a sans serif or even a serif font, Romantically is an excellent choice for a personal logotype. </p>
+                <h4>Romantically offers – </h4>
+
+                <ul>
+                    <li>Regular and italic version</li>
+                    <li>Language support</li>
+                    <li>417 ligatures</li>
+                    <li>Swashes</li>
+                </ul>
+                <h3>5. Little Love – Modern Calligraphy Font</h3>
+                <img src="images\25BestHandwriting5.jpg" alt="">
+                <p>Fresh and modern, this cute handwriting font is ideal for pairing two names with a heart. Use it on your greeting cards, posters, wedding invitations, quotes, business cards, mugs, branding materials, gifts, and much more.</p>
+                <h4>Little Love includes – </h4>
+                <ul>
+                    <li>2 fonts (bold & regular)</li>
+                    <li>help file (with tutorials)</li>
+                    <li>repertoire file</li>
+                    <li>.OTF, .TTF and .WOFF</li>
+                    <li>Includes swashes and ligatures</li>
+                </ul>
+
+                <h3>6. The Mozart Script – Elegant And Classy</h3>
+                <img src="images\25BestHandwriting6.jpg" alt="">
+                <p>If you are a fan of the traditional calligraphy look like me, this font is a perfect choice for you. Early American calligraphy styles inspire the Mozart Script. Formal, gracious, and beautiful are just some ways to describe it. Mozart isn’t just a beautiful handwriting font, it’s also jam-packed with a ton of extras. </p>
+                <p>This set includes – </p>
+                <p>• MozartScript .TTF with four weights: Thin, Regular, Bold, and Black with an extra thin mainline.</p>
+                <p>• MozartScriptExt .TTF – a bold version of MozartScript.TTF.</p>
+                <p>• MozartScriptCutOff .TTF – an option for special programs like MS Word, which do not support OpenType features.</p>
+                <p>• MozartScript text file with download links for designers who use special programs like Silhouette or similar programs which do not show the entire font family.</p>
+                <p>• Contextual alternates</p>
+                <p>• Discretionary ligatures</p>
+                <p>• Stylistics alternates (600+)</p>
+                <p>• Compatible with Silhouette & Circuit Design Space</p>
+                <p>• Help .PDF file with the most common questions and answers.</p>
+                
+                <h3>7. Lenka Morris – Stylish Handwriting Font</h3>
+                <img src="images\25BestHandwriting7.jpg" alt="">
+                <p>On the other side of the spectrum, we have Lenka Morris. A messy and stylish handwriting font designed to add pizazz to your designs and projects. The set includes 57 unique ligatures different from the primary alphabet and nine distinctive icons in the PUA code. This signature-style handwriting font also contains a help file that tells you how to access and use it.</p>
+
+                <h3>8. Beauty Anthena – Beautiful Calligraphy Font</h3>
+                <img src="images\25BestHandwriting8.jpg" alt="">
+                <p>Beauty Anthena has a classic decorative copper script with a modern touch. Designed with great detail to bring stylish elegance and a clean and feminine look, this calligraphy handwriting font is an excellent choice for modern minimalism and classic expert penmanship styles.</p>
+                <h4>In this font set, you will find – </h4>
+                <ul>
+                    <li>Beauty-Athena font (OTF,TTF, and WOFF format)</li>
+                    <li>Opentype feature: Ligature and nine stylistic set alternates</li>
+                </ul>
+
+                <h3>9. Brachetto – Traditional Script Font</h3>
+                <img src="images\25BestHandwriting9.jpg" alt="">
             </div>
         </div>
         <p></p>
